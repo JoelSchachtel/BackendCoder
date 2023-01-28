a7d482f41f87bf0e5ca5cd66bc9706814c5e04d7
JoelSchachtel <Joelschachtel@hotmail.com>
2023-01-28 12:09:31 -0300

    Mongoose and socket

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
 import express from 'express';
-import productRouter from './routes/products.router.js';
+import handlebars from 'express-handlebars';
+import { Server } from 'socket.io';
+import __dirname from './utils.js';
+import mongoose from 'mongoose';
+// import productRouter from './routes/products.router.js';
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
-app.use('/static', express.static('public'))
-app.use('/api/products', productRouter)
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
 
-app.listen(8080, () => {
-    console.log('Servidor funcionando en puerto 8080...');
+mongoose.connect("mongodb+srv://admin:admin@cluster0.p4oohjo.mongodb.net/?retryWrites=true&w=majority", {
+    dbName: "ecommerce"
+}, (error) => {
+    if(error) {
+        console.log('DB no conected...');
+        return
+    };
+    const httpServer = app.listen(8080, () => console.log('Listening in port 8080...'));
+    const socketServer = new Server(httpServer);
+    httpServer.on('error', () => console.log('error'));
+    return(socketServer, app)
 })
